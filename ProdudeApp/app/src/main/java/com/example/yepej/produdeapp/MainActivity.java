c25fae77d793f27ad827f3e1ac7132b6cfa8dc76
jyepe <yepejy@gmail.com>
2018-06-26 12:32:50 -0400

    Checks if if login is correct and sign up page added

--- ProdudeApp/app/src/main/java/com/example/yepej/produdeapp/MainActivity.java
+++ ProdudeApp/app/src/main/java/com/example/yepej/produdeapp/MainActivity.java
@@ -1,75 +1,18 @@
 package com.example.yepej.produdeapp;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity
 {
-
-    public class SendPostData extends AsyncTask<String, Void, String>
-    {
-
-        @Override
-        protected String doInBackground(String... params)
-        {
-            // TODO: 6/25/2018 fix this messy code 
-            
-            try
-            {
-                String text = "";
-                String data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode("Julian", "UTF-8");
-                BufferedReader reader = null;
-                URL url = new URL("http://192.168.1.109/ds.php");
-                URLConnection conn = url.openConnection();
-                conn.setDoOutput(true);
-                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-                wr.write( data );
-                wr.flush();
-
-                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                // Read Server Response
-                while((line = reader.readLine()) != null)
-                {
-                    // Append server response in string
-                    sb.append(line + "\n");
-                }
-
-
-                text = sb.toString();
-                Log.i("test", text);
-
-            } catch (Exception e) 
-            {
-                System.out.println(e.getMessage());
-            }
-            
-            return null;
-        }
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -78,10 +21,63 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    public void buttonClicked(View control) throws IOException
+    public void signUpClicked(View control)
     {
-        SendPostData sendPostData = new SendPostData();
-
-        sendPostData.execute("http://192.168.1.109/ds.php");
+        setContentView(R.layout.sign_up);
     }
+
+    //region Login
+    public void loginClick(View control)
+    {
+        TextView username = ((TextView) findViewById(R.id.usernameText));
+        TextView password = ((TextView) findViewById(R.id.passwordText));
+
+        if (username.getText().toString().equals("") || password.getText().toString().equals(""))
+        {
+            Toast.makeText(this, "Username or Password missing", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            sendLoginInfo(username, password);
+        }
+    }
+
+    private void sendLoginInfo(TextView username, TextView password)
+    {
+        PostSender sendPostData = new PostSender();
+
+        try
+        {
+            String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username.getText().toString(), "UTF-8");
+
+            data += "&" + URLEncoder.encode("password", "UTF-8") + "="
+                    + URLEncoder.encode(password.getText().toString(), "UTF-8");
+
+            data += "&" + URLEncoder.encode("method", "UTF-8") + "="
+                    + URLEncoder.encode("login", "UTF-8");
+
+            String serverResponse = sendPostData.execute("http://192.168.1.109/ds.php", data).get();
+            Log.i("test", serverResponse);
+            checkLoginResponse(serverResponse);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkLoginResponse(String serverResponse)
+    {
+        if (serverResponse.contains("false"))
+        {
+            Toast.makeText(this, "Incorrect login information", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Login successful", Toast.LENGTH_LONG).show();
+        }
+    }
+    //endregion
+
+
 }
--- ds.php
+++ ds.php
@@ -1,35 +1,60 @@
 <?php
-	$var = urldecode($_POST['name']) ;
-	
-	print " ==== POST DATA =====
-       Name  : $var";
 
-
-
-    //Info to connect to DB
+	//Info to connect to DB
 	$servername = "localhost";
-	$username = "jyepe";
-	$password = "9373yepe";
+	$dbusername = "jyepe";
+	$dbpassword = "9373yepe";
 	$dbname = "mydb";
 
+	//what method to execute
+	$method = urldecode($_POST['method']) ;
+
+	
+
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 	// Check connection
 	if ($conn->connect_error) 
 	{
 	    die("Connection failed: " . $conn->connect_error);
 	}
+	else
+	{
+		echo "success";
+	}
+
 	
 
-	$sql = "INSERT INTO FAVORITES (CUSTOMER, ITEM) 
-						VALUES (1, 2)";
+	
+	function checkLogin ()
+	{
+		global $conn;
+		$username = urldecode($_POST['username']) ;
+		$password = urldecode($_POST['password']) ;
+		
+
+		$sql = "SELECT * FROM CUSTOMERS WHERE PASSWORD = $password AND UID = $username";
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0)
+		{
+			echo "true";
+		}
+		else
+		{
+			echo "false";
+		}
+
+		$conn->close();
+
+	}
 
 
-	if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-	$conn->close();
+
+	if ($method == 'login')
+	{
+		checkLogin();
+	}
+
 ?>
