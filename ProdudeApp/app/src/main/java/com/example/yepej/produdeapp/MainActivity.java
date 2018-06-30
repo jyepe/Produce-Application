bbd77054888a8ba96f8aaa6168578b7c2cbccc7e
jyepe <yepejy@gmail.com>
2018-06-30 19:22:10 -0400

    Spinner has list of states

--- ProdudeApp/app/src/main/java/com/example/yepej/produdeapp/MainActivity.java
+++ ProdudeApp/app/src/main/java/com/example/yepej/produdeapp/MainActivity.java
@@ -34,13 +34,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //create a list of items for the spinner.
-        String[] states = new String[]{"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC",
-                "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",
-                "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",
-                "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",
-                "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"};
     }
 
 
